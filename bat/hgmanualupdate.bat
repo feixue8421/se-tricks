c3b2a5a537f0cbeb2dd86f8faeab21181d6b7907
Wu <yong.5.wu@nokia-sbell.com>
2019-12-06 15:20:17 +0800

    add hg manual update command for windows

---
+++ bat/hgmanualupdate.bat
@@ -0,0 +1,20 @@
+@ECHO OFF
+SETLOCAL
+
+REM hg manual update to avoid file collision on windows.
+
+ECHO Start manual update ...
+
+IF "%~1" == "" (
+    SET rev=tip
+) ELSE (
+    SET rev=%~1
+)
+
+hg debugsetparents %rev%
+hg debugrebuildstate
+hg revert -I **/*.h -I **/*.hh -I **/*.hpp -I **/*.c -I **/*.cc -I **/*.cpp -I **/makefile -I **/*.xml -I **/*.json
+
+ECHO Finish manual update!!
+ENDLOCAL
+EXIT /B 0
--- bat/hgpreupdate.bat
+++ bat/hgpreupdate.bat
@@ -7,11 +7,24 @@ SET PYTHON=C:\Users\y6wu\AppData\Local\Programs\Python\Python37\python.exe
 SET PREUPDATE=C:\Repository\se-tricks\python\hgext\preupdate.py
 
 ECHO Start to check file collision...
-hg manifest tip | %PYTHON% %PREUPDATE%
-IF %ERRORLEVEL% NEQ 0 (
-    ECHO Errors detected during checking, errorlevel %ERRORLEVEL%
-    ENDLOCAL
-    EXIT /B 1
+REM hg manifest tip | %PYTHON% %PREUPDATE%
+REM IF %ERRORLEVEL% NEQ 0 (
+REM     ECHO Errors detected during checking, errorlevel %ERRORLEVEL%
+REM     ENDLOCAL
+REM     EXIT /B 1
+REM )
+
+SET HG="C:\Program Files (x86)\Mercurial\hg.exe"
+
+%HG% debugsetparents tip
+%HG% debugrebuildstate
+
+FOR /F "usebackq tokens=*" %%A IN (`%HG% manifest tip`) DO (
+    IF EXIST %%A (
+        ECHO %%A exist, and not update
+    ) ELSE (
+        hg revert %%A
+    )
 )
 
 ECHO Finish checking file collision!!
