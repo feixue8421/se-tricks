3d2aa143533488a57c2e98094a6f4de4ea9fe4ad
Wu <yong.5.wu@nokia-sbell.com>
2019-12-27 10:24:35 +0800

    powershell script refactor and .vimrc added

--- powershell/synchronise.ps1
+++ powershell/synchronise.ps1
@@ -1,8 +1,34 @@
 "synchronise files between linux server and local backups"
 
-"synchronise bashrc ..."
-pushd "C:\Repository\se-tricks\shell"
-scp yongwu@172.24.213.197:~/.bashrc work.bashrc
+$server="yongwu@172.24.213.197"
+$files=".bashrc",".vimrc"
+$prefix="work"
+
+pushd "c:\repository\se-tricks\shell"
+
+foreach ($file in $files)
+{
+    "-----------------------------------------------"
+    "synchronise $file ..."
+    $local=get-filehash "work$file" -algorithm MD5
+    ($local -match "(?<md5>[0-9a-zA-Z]{32})") | out-null
+    $local = $matches['md5']
+    "local: $local"
+
+    $remote=ssh $server md5sum "~/$file"
+    "remote: $remote"
+
+    if ($remote -imatch $local)
+    {
+        "no need to update $file"
+    }
+    else
+    {
+        scp ($server + ":~/$file") "$prefix$file"
+    }
+    "-----------------------------------------------"
+}
+
 popd
 
 "synchronise done !!"
